ae3d7310411b431d0fcfebfff6d7bd7d2aa9c21b
TonNom <TonEmail@example.com>
2025-06-11 17:17:37 +0000

    🎉 Initial commit - version stable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
-import { loadNeosaasConfig } from "@/server/loadConfig"
+import { loadConfig as loadNeosaasConfig } from '@/server/loadConfig'
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
