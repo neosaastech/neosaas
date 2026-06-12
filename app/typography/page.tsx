dee9536482aab06d2d38082b8849e3b9d4ad3ab7
Charles Vandendriessche <chvandendriessche@neomnia.net>
2026-06-12 08:45:31 +0200

    chore: remove LLM-generated stub page

--- app/typography/page.tsx
+++
@@ -1,9 +0,0 @@
-import TypographySection from '@/components/Typography/TypographySection';
-
-export default function TypographyPage() {
-  return (
-    <main className="min-h-screen bg-background">
-      <TypographySection />
-    </main>
-  );
-}
